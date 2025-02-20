21347b33db166dc91209f731dc35375cd4f08530
Buenaparte <131205534+Buenaparte@users.noreply.github.com>
2025-02-20 19:52:44 -0400

    Update Sistema.java

--- Simulacion Planificacion/src/main/Sistema.java
+++ Simulacion Planificacion/src/main/Sistema.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
+//NetBeans
 
 //clase principal que ordena y gestiona a las cpus y sus procesos
 package main;
