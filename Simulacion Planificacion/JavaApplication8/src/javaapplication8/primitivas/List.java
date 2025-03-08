0680bb7592d43bf65043019211dcdc6ac47849a1
Buenaparte <131205534+Buenaparte@users.noreply.github.com>
2025-03-07 22:55:12 -0400

    Version final
    ya te entrego mi parte, solo falta que hagas lo que queda

--- Simulacion Planificacion/JavaApplication8/src/javaapplication8/primitivas/List.java
+++ Simulacion Planificacion/JavaApplication8/src/javaapplication8/primitivas/List.java
@@ -2,14 +2,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package primitivas;
-
-//import java.util.HashSet;
+package javaapplication8.primitivas;
 
 /**
  *
  * @author RDG
- * @param <T>
  */
 public class List<T>{
     //Atributos
@@ -30,29 +27,9 @@ public class List<T>{
     public boolean isEmpty(){
         return first == null;
     }
-
-    public MyNode<T> getFirst() {
-        return first;
-    }
-
-    public MyNode<T> getLast() {
-        return last;
-    }
-
-    public MyNode<T> getNext() {
-        return next;
-    }
-      
+    
     public int isSize(){
         return size;
-    }
-
-    public void setSize(int size) {
-        this.size = size;
-    }
-
-    public void setFirst(MyNode<T> first) {
-        this.first = first;
     }
     
     public void empty(){
@@ -146,14 +123,14 @@ public class List<T>{
     
     public void delStart(){
         if(!this.isEmpty()){
-            if (this.getNext() == null) {
-                System.out.println("entre en la zona de muerte");
+            if (size == 1) {
                 this.empty();
             }
             else{
                 first = first.getNext();
                 first.setPrev(null);
                 
+                size--;
             }
         }
     }
@@ -166,6 +143,7 @@ public class List<T>{
             else{
                 last = last.getPrev();
                 last.setNext(null);
+                size--;
             }
         }
     }
@@ -304,9 +282,6 @@ public class List<T>{
             while (i != pos){
 //                System.out.println("I");
 //                System.out.println(i);
-                if (aux.getNext() == null){
-                    break;
-                }
                 aux = aux.getNext();
                 i++;
             }
@@ -463,117 +438,4 @@ public class List<T>{
             temp = temp.getNext();
         }
     }
-    
-    
-    //busca un node en el valor del numero indicado
-    public Object search(int numero){
-      MyNode pointer = getFirst(); 
-        int a = 0;
-                if(numero == 0){
-            return getFirst().getData();
-        }
-        while(pointer.getNext() != null && a<numero ){
-            pointer = pointer.getNext();
-            a++;
-        }
-        if (numero == isSize()){
-           return getLast().getData();
-        }else{
-            return pointer.getElement();
-        }  
-    }
-    
-    //metodo para llenar las listas sin aumentar el size
-   public void llenar(T data){     
-       MyNode<T> newData = new MyNode(data);
-//        System.out.println("Data "+ data);
-//        System.out.println("NewData "+ newData);
-        if(this.isEmpty()){
-            newData.setData(data);
-            first = last = newData;
-        }else{
-            newData.setData(data);
-            last.setNext(newData);
-            newData.setPrev(last);
-            last = newData;
-
-        }   
-     } 
-   
-   
-   //metodo para revisar si hay espacio en la lista de cpus para un nuevo proceso
-   public boolean Revisar(int a){
-      MyNode temp = first;
-      int i = 0;
-      while (i < a){
-        if (temp != null){  
-         temp = temp.getNext();
-         i++;
-      }else{
-          return true;
-        }        
-   }
-      return false;
- }
-     //metodo de delete que funciona correctamente en bucle
-       public void deleteBegin(){
-        if (isEmpty()) {
-            System.out.println("La lista esta vacia");
-        } else {
-            MyNode pointer = getFirst();
-            setFirst(pointer.getNext());
-            pointer.setNext(null);         
-        }
-        size--;
-    }
-       
-          public void deleteFinal(){
-        if (isEmpty()) {
-            System.out.println("La lista esta vacia");
-        } else {
-            MyNode pointer = getFirst();
-            while (pointer.getNext().getNext() != null) {
-                pointer = pointer.getNext();
-            }
-            pointer.setNext(null);
-            size--;
-        }
-    }     
-       
-  public void Borrar_especifico(Object camino){
-         if (isEmpty()) {
-            System.out.println("La lista esta vacia");
-        } else {
-                      size--;
-            MyNode pointer = getFirst();
-            boolean encontrado = false;
-            if(camino == getFirst().getElement()){
-                deleteBegin();
-                pointer = getFirst();
-            }
-            if(camino == getLast().getElement()){
-                deleteFinal();
-                pointer = getLast();
-            }
-            while(pointer != null && encontrado == false){
-                if(pointer.getElement() == camino){
-                    MyNode temp = pointer.getPrev();
-                    MyNode temp2 = pointer.getNext();
-                    if(temp2 == null){
-                        deleteFinal();
-                    }else{
-                    temp.setNext(temp2);
-                    temp2.setPrev(temp); 
-                    }
-                    pointer.setPrev(null);
-                    pointer.setNext(null);
-                    size--;
-                    encontrado = true;
-                }else{
-                    pointer = pointer.getNext();
-                }
-            }
-        }
-  }   
-   
 }
--- Simulacion Planificacion/JavaApplication8/src/javaapplication8/primitivas/MyNode.java
+++ Simulacion Planificacion/JavaApplication8/src/javaapplication8/primitivas/MyNode.java
@@ -2,12 +2,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package primitivas;
+package javaapplication8.primitivas;
 
 /**
  *
  * @author RDG
- * @param <T>
  */
 public class MyNode<T> {
     //Atributos
